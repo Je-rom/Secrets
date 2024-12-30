166edcf65c7972f72e1aa51bd279e6bb86a6947b
Jerry Abadi <abadijerry44@gmail.com>
2024-12-30 11:10:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({
-    secret: "Obv Messi is the GOAT.",
+    secret: "",
     resave: false,
     saveUninitialized: false
   }));
